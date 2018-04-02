95ff6c78a135a1b87e2f04bc1e59a81eba4aa89e
product <2358630014@qq.com>
2018-04-02 12:41:24 +0800

    环境配置更新

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  serverDomain: "https://www.pinree.cn/",
+  serverDomain: "https://www.aimipu.co/",
   appDomain:"http://b-webtest.idaoying.com/#/",
   qiniuDomain:"http://p22g08bt2.bkt.clouddn.com/",
 };
